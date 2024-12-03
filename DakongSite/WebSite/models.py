3bc8649e6ac51c9ee52c52d78e14c6786481f1a5
Siganmorfu <hu881121@gmail.com>
2024-12-04 08:56:11 +0900

    modify toolbar

--- DakongSite/WebSite/models.py
+++ DakongSite/WebSite/models.py
@@ -1,15 +1,49 @@
+# WebSite/models.py
 from django.db import models
 from parler.models import TranslatableModel, TranslatedFields
-# Create your models here.
-# WebSite/models.py (Example of Static Page Model)
 
+# HomePageContent model to store homepage information
+# WebSite/models.py
+from django.db import models
+from parler.models import TranslatableModel, TranslatedFields
 
+# HomePageContent model to store homepage information
 class HomePageContent(TranslatableModel):
     translations = TranslatedFields(
-        headline=models.CharField(max_length=200),
-        subheadline=models.TextField(),
+        headline=models.CharField(max_length=200),  # Title of the homepage section
+        subheadline=models.TextField(),  # Subtitle or detailed description
     )
-    promotional_image = models.ImageField(upload_to='homepage/')
+    promotional_image = models.ImageField(upload_to='homepage/')  # Image for promotional purposes
 
     def __str__(self):
         return self.safe_translation_getter('headline', any_language=True)
+
+
+# Toolbar model to manage toolbar links and labels
+class Toolbar(TranslatableModel):
+    APP_LINKS = {
+        'home': '/',
+        'news': '/news/',
+        'products': '/products/',
+        'media': '/media/library/',
+        'account': '/account/login/',
+    }
+    translations = TranslatedFields(
+        label=models.CharField(max_length=100),  # Label for the toolbar link
+    )
+    link = models.CharField(max_length=100, choices=[(key, key.capitalize()) for key in Toolbar.APP_LINKS.keys()])  # URL link for the toolbar item
+    order = models.PositiveIntegerField(default=0)  # Order of the toolbar item
+
+    class Meta:
+        ordering = ['order']  # Ensure toolbar items are displayed in the specified order
+
+    def __str__(self):
+        return self.safe_translation_getter('label', any_language=True)
+
+    # Method to render toolbar links in HTML
+    def to_html(self):
+        link_url = self.APP_LINKS.get(self.link, self.link)
+        return f'<li><a href="{link_url}">{self.safe_translation_getter("label", any_language=True)}</a></li>'
+
+
+
--- DakongSite/WebSite/views.py
+++ DakongSite/WebSite/views.py
@@ -7,6 +7,28 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
+# WebSite/views.py
+from django.shortcuts import render
+from .models import HomePageContent, Toolbar
+from News.models import Article
+from Products.models import Product
+
+def homepage(request):
+    # Fetch the latest homepage content, news, products, and toolbar items
+    homepage_content = HomePageContent.objects.all().first()
+    latest_news = Article.objects.language().all()[:3]
+    featured_products = Product.objects.language().all()[:3]
+    toolbar_items = Toolbar.objects.all()
+
+    context = {
+        'homepage_content': homepage_content,
+        'latest_news': latest_news,
+        'featured_products': featured_products,
+        'toolbar_items': toolbar_items,
+    }
+    return render(request, 'website/home.html', context)
+
 def homepage(request):
     logger.debug("Fetching homepage content, news, and products.")
     # Fetch the latest homepage content, news, and products
