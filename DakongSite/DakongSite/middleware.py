0204e7270ee43abe7c1d670c157dd039d3402bea
Siganmorfu <hu881121@gmail.com>
2024-12-19 02:57:23 +0900

    fix(Dakong.midware): fix /admin/ compare bug

--- DakongSite/DakongSite/middleware.py
+++ DakongSite/DakongSite/middleware.py
@@ -11,10 +11,14 @@ class RedirectUnmatchedURLMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        # 如果路徑是 /swagger/ 或 /admin/，不進行重定向 # TODO: move 邏輯錯誤 應該不能有額外
+        print(f"Request Path Info: {request.path_info}")
+
+        if request.path_info.startswith('/admin/'):
+            return self.get_response(request)
         try:
-            # 嘗試解析當前的路徑
-            resolve(request.path_info)
+            resolve(request.path_info)  # 嘗試解析路由
         except Resolver404:
-            # 如果路由未匹配，重定向到 /swagger/
-            return redirect('/swagger/')
+            return redirect('/swagger/')  # 未匹配則重定向到 /swagger/
         return self.get_response(request)
+
